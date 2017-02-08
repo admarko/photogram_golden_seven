7ba6e23ace2fe9787807bb0ead7664055904ed76
Alex Markowitz <admarko@gmail.com>
2017-02-07 22:08:10 -0600

    halfway done
    a

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -5,5 +5,32 @@ class PhotosController < ApplicationController
     render("photos/index.html.erb")
   end
 
+  def show
+    @id = params["id"]
+    @source = Photo.find(@id).source
+    @caption = Photo.find(@id).caption
+
+    render("photos/show.html.erb")
+  end
+
+  def new_form
+    render("photos/create.html.erb")
+  end
+
+  def create_row
+    p = Photo.new
+    p.caption = params[:the_caption]
+    p.source = params[:the_source]
+    p.save
+
+    redirect_to("http://localhost:3000/photos")
+  end
+
+  def destroy
+    p = Photo.find(params["id"])
+    p.destroy
+
+    redirect_to("http://localhost:3000/photos")
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,20 @@ Rails.application.routes.draw do
 
   get("/", { :controller => "photos", :action => "index" })
 
-  # Routes to READ photos
+
+  #CREATE
+  get("/photos/new",       { :controller => "photos", :action => "new_form" })
+  get("/create_photo",     { :controller => "photos", :action => "create_row" })
+
+  #READ
   get("/photos",           { :controller => "photos", :action => "index" })
   get("/photos/:id",       { :controller => "photos", :action => "show" })
+
+  #UPDATE
+
+  #DELETE
+  get("/delete_photo/:id", { :controller => "photos", :action => "destroy" })
+
+
 
 end
